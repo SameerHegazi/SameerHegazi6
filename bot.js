77399a6d084f41256ec0a6bfa9637e81990e0b0a
SameerHegazi <40927890+SameerHegazi@users.noreply.github.com>
2019-01-04 21:20:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,6 @@ const Discord = require('discord.js');
 const moment = require("moment");
 const fs = require("fs");
 const client = new Discord.Client();
- const prefix = "-";
-client.on('ready', () => {
-    console.log('I am ready!');
-});
 
 client.on('message', message => {
   var prefix = "#"; 
@@ -28,5 +24,3 @@ message.delete()
 
 
 client.login(process.env.BOT_TOKEN);
-
-
