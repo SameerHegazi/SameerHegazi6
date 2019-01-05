47027e8bb626e6d285252d22778ab1066e21f159
SameerHegazi <40927890+SameerHegazi@users.noreply.github.com>
2019-01-05 06:53:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(` BODY MAN');
   console.log('---------------');
   console.log('Desert Bot Is Online')
   console.log('---------------')
